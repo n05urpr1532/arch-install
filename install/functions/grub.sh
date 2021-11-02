c1778f596a12c3ad72829a6cc2b4038a2aeeb24f
JPV <john.doe@no-email.net>
2021-11-02 22:24:19 +0100

    feat(scripts): Limit GRUB GFX mode to 2K

--- install/functions/grub.sh
+++ install/functions/grub.sh
@@ -9,6 +9,6 @@ configure_grub_theme () {
   mkdir -p /mnt/boot/grub/themes
   cp -pr "$(get_directory 'grub-themes/Xenlism-Arch')" /mnt/boot/grub/themes/
 
-  sed -i 's@GRUB_GFXMODE=auto@GRUB_GFXMODE=3840x2160,2560x1440,1920x1200,1920x1080@' /mnt/etc/default/grub
+  sed -i 's@GRUB_GFXMODE=auto@GRUB_GFXMODE=2560x1440,1920x1200,1920x1080@' /mnt/etc/default/grub
   sed -i 's@#GRUB_THEME="/path/to/gfxtheme"@GRUB_THEME="/boot/grub/themes/Xenlism-Arch/theme.txt"@' /mnt/etc/default/grub
 }
